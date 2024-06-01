f8ab5837416efeb1ae2bd0248b2312a65c1f0245
Fatima FRIST <116951146+FatimaFrist@users.noreply.github.com>
2024-06-01 23:56:41 +0100

    Update detect_emotions_vd.py

--- detect_emotions_vd.py
+++ detect_emotions_vd.py
@@ -65,7 +65,7 @@ def analyze_video_emotions(video_path, output_video_folder, output_data_folder):
     # Créer un objet VideoWriter pour enregistrer la vidéo modifiée dans le dossier de sortie avec le même nom de fichier
     output_path = os.path.join(output_video_folder, f'{video_name}.mp4')
     out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
-    st.write(f"Emotion video path: {out}")
+    print(f"Emotion video path: {out}")
     # Initialiser le dictionnaire pour compter les émotions
     total_emotions_count = {emotion: 0 for emotion in EMOTIONS}
     total_faces_detected = 0
